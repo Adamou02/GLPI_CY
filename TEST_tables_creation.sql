1189776032481b2b74c251d85edfb7ae39a2cb05
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2024-03-19 14:10:11 +0100

    nouvelles colonnes + contraintes

--- TEST_tables_creation.sql
+++ TEST_tables_creation.sql
@@ -1,71 +1,87 @@
--- Table de rï¿½fï¿½rence contient une liste prï¿½dï¿½finie de valeurs pour les prioritï¿½s des commentaires. Elle garantit la cohï¿½rence dans le niveau de prioritï¿½ attribuï¿½ ï¿½ un commentaire. 
+-- Table de référence contient une liste prédéfinie de valeurs pour les priorités des commentaires. Elle garantit la cohérence dans le niveau de priorité attribué à un commentaire. 
 
 CREATE TABLE GLPI_TEST.REF_priority (
     priority_id INT PRIMARY KEY,
-    priority VARCHAR2(10) CHECK (priority IN ('Very low', 'Low', 'Medium', 'High', 'Very high'))
+    "priority" VARCHAR2(10) NOT NULL UNIQUE
 );
 
--- Table de rï¿½fï¿½rence qui permet de dï¿½finir une liste prï¿½dï¿½finie de valeurs pour les statuts du ticket.
--- priority est une VARCHAR2(10). Le CHECK nous permet de connaï¿½tre ï¿½ lï¿½avance les champs possible pour cette colonne, et donc dï¿½allouer un espace de stockage optimisï¿½.
+-- Table de référence qui permet de définir une liste prédéfinie de valeurs pour les statuts du ticket.
+-- priority est une VARCHAR2(10). Le CHECK nous permet de connaître à l’avance les champs possible pour cette colonne, et donc d’allouer un espace de stockage optimisé.
 
 CREATE TABLE GLPI_TEST.REF_status (
     status_id INT PRIMARY KEY,
-    status VARCHAR2(50)
+    "status" VARCHAR2(50) NOT NULL UNIQUE
 );
 
--- Table de rï¿½fï¿½rence qui permet de dï¿½finir une liste prï¿½dï¿½finie de catï¿½gories pour les tickets.
+-- Table de référence qui permet de définir une liste prédéfinie de catégories pour les tickets.
  
 CREATE TABLE GLPI_TEST.REF_category (
     category_id INT PRIMARY KEY,
-    category VARCHAR2(50)
+    "category" VARCHAR2(50) NOT NULL UNIQUE
 );
 
--- Table de rï¿½fï¿½rence qui permet de dï¿½finir une liste prï¿½dï¿½finie de types pour les tickets. 
+-- Table de référence qui permet de définir une liste prédéfinie de types pour les tickets. 
 
 CREATE TABLE GLPI_TEST.REF_type (
     type_id INT PRIMARY KEY,
-    type VARCHAR2(50)
+    "type" VARCHAR2(50) NOT NULL UNIQUE
 );
 
--- Table de rï¿½fï¿½rence qui permet de dï¿½finir une liste prï¿½dï¿½finie de rï¿½les pour les tickets. 
+-- Table de référence qui permet de définir une liste prédéfinie de rôles pour les tickets. 
 
 CREATE TABLE GLPI_TEST.REF_role (
     role_id INT PRIMARY KEY,
-    role VARCHAR2(50)
+    "role" VARCHAR2(50) NOT NULL UNIQUE
 );
 
--- Table de rï¿½fï¿½rence qui permet de dï¿½finir une liste prï¿½dï¿½finie de groupes pour les tickets. 
-
-CREATE TABLE GLPI_TEST.REF_group (
-    group_id INT PRIMARY KEY,
-    "group" VARCHAR2(50)
-);
--- La colonne ï¿½groupï¿½ est entre guillemets pour quï¿½Oracle ne la confonde pas avec la commande sql GROUP
-
-
-
--- Table qui stocke les dï¿½tails sur les emplacements physiques associï¿½s aux tickets.
+-- Table qui stocke les détails sur les emplacements physiques associés aux tickets.
 
 CREATE TABLE GLPI_TEST.LOCATIONS (
     location_id INT PRIMARY KEY,
-    location VARCHAR2(70)
+    country VARCHAR2(50),
+    city VARCHAR2(50),
+    address VARCHAR2(50),
+    address_number INT,
+    sub_group VARCHAR2(50),
+    "location" VARCHAR2(70)
 );
 
+-- Table de référence qui permet de définir une liste prédéfinie de groupes pour les tickets. 
 
---  Table qui stocke les dï¿½tails sur le matï¿½riel concernï¿½ par les tickets.
+CREATE TABLE GLPI_TEST.REF_group (
+    group_id INT PRIMARY KEY,
+    "group" VARCHAR2(50),
+    fk_location INT,
+    FOREIGN KEY (fk_location) REFERENCES GLPI_TEST.LOCATIONS(location_id)
+);
+-- La colonne “group” est entre guillemets pour qu’Oracle ne la confonde pas avec la commande sql GROUP
+
+--  Table qui stocke les détails sur le matériel concerné par les tickets.
 
 CREATE TABLE GLPI_TEST.HARDWARES (
     hardware_id INT PRIMARY KEY,
-    name VARCHAR2(50)
+    "name" VARCHAR2(50),
+    brand VARCHAR2(50),
+    purchase_date TIMESTAMP
 );
 
 
--- Table qui stocke les informations sur les utilisateurs de lï¿½outil de ticketing
+-- Table qui stocke les informations sur les utilisateurs de l’outil de ticketing
 
 CREATE TABLE GLPI_TEST.USERS (
     user_id INT PRIMARY KEY,
     fk_role INT,
     fk_group INT,
+    "password" VARCHAR2(255) NOT NULL
+    CHECK ( 
+        LENGTH("password") >= 14
+        AND REGEXP_LIKE("password", '[0-9]')
+        AND REGEXP_LIKE("password", '[A-Z]')
+        AND REGEXP_LIKE("password", '[a-z]')
+        AND REGEXP_LIKE("password", '[[:punct:]]')
+    ),
+    email VARCHAR2(50) NOT NULL UNIQUE
+        CHECK(REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')),
     last_name VARCHAR2(50),
     first_name VARCHAR2(50),
     company VARCHAR2(50),
@@ -74,11 +90,11 @@ CREATE TABLE GLPI_TEST.USERS (
     FOREIGN KEY (fk_group) REFERENCES GLPI_TEST.REF_group(group_id),
     FOREIGN KEY (fk_location) REFERENCES GLPI_TEST.LOCATIONS(location_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
 
 
 
--- C'est la table principale qui contient les dï¿½tails de chaque ticket de support.
+-- C'est la table principale qui contient les détails de chaque ticket de support.
 
 CREATE TABLE GLPI_TEST.TICKETS (
     ticket_id INT PRIMARY KEY,
@@ -106,13 +122,13 @@ CREATE TABLE GLPI_TEST.TICKETS (
     FOREIGN KEY (fk_category) REFERENCES GLPI_TEST.REF_category(category_id),
     FOREIGN KEY (fk_hardwares) REFERENCES GLPI_TEST.HARDWARES(hardware_id)
 );
--- title est un VARCHAR2(100) et permettent de titrer les tickets, nous avons rï¿½duit la taille du VARCHAR2 pour optimiser lï¿½espace de stockage. 100 caractï¿½res nous semble ï¿½tre suffisant pour stocker un titre.
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
--- ressources est un CLOB (Character Large Object). Cela permet de stocker des string de plus de 4000 caractï¿½res. Cela est nï¿½cessaire pour nos images stockï¿½es en base64.
+-- title est un VARCHAR2(100) et permettent de titrer les tickets, nous avons réduit la taille du VARCHAR2 pour optimiser l’espace de stockage. 100 caractères nous semble être suffisant pour stocker un titre.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
+-- ressources est un CLOB (Character Large Object). Cela permet de stocker des string de plus de 4000 caractères. Cela est nécessaire pour nos images stockées en base64.
 -- TIMESTAMP est un type qui permet de stocker une date et heure en SQL.
 
 
--- Table stocke les commentaires associï¿½s ï¿½ chaque ticket.
+-- Table stocke les commentaires associés à chaque ticket.
 
 CREATE TABLE GLPI_TEST.COMMENTS (
     comment_id INT PRIMARY KEY,
@@ -126,12 +142,12 @@ CREATE TABLE GLPI_TEST.COMMENTS (
     FOREIGN KEY (fk_ticket) REFERENCES GLPI_TEST.TICKETS(ticket_id),
     FOREIGN KEY (fk_user) REFERENCES GLPI_TEST.USERS(user_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
--- task est un VARCHAR2(255) et de dï¿½crire la tï¿½che liï¿½ ï¿½ la rï¿½solution dï¿½un ticket. 255 caractï¿½res nous semble ï¿½tre suffisant pour stocker cette information.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
+-- task est un VARCHAR2(255) et de décrire la tâche lié à la résolution d’un ticket. 255 caractères nous semble être suffisant pour stocker cette information.
 
 
 
--- Table qui stocke les ressources disponibles.Les ressources peuvent ï¿½tre des fichiers, des liens, ou d'autres types de documents qui peuvent ï¿½tre associï¿½s ï¿½ des tickets ou des commentaires.
+-- Table qui stocke les ressources disponibles.Les ressources peuvent être des fichiers, des liens, ou d'autres types de documents qui peuvent être associés à des tickets ou des commentaires.
 
 CREATE TABLE GLPI_TEST.RESSOURCES (
     ressource_id INT PRIMARY KEY,
@@ -139,11 +155,11 @@ CREATE TABLE GLPI_TEST.RESSOURCES (
     ressource CLOB,
     FOREIGN KEY (fk_ticket) REFERENCES GLPI_TEST.TICKETS(ticket_id)
 );
--- ressources est un CLOB (Character Large Object). Cela permet de stocker des string de plus de 4000 caractï¿½res. Cela est nï¿½cessaire pour nos images stockï¿½es en base64.
+-- ressources est un CLOB (Character Large Object). Cela permet de stocker des string de plus de 4000 caractères. Cela est nécessaire pour nos images stockées en base64.
 
 
 
--- Table de liaison entre les tickets et les ressources. Elle permet de gï¿½rer la relation 1-n entre les tickets et les ressources.
+-- Table de liaison entre les tickets et les ressources. Elle permet de gérer la relation 1-n entre les tickets et les ressources.
 
 CREATE TABLE GLPI_TEST.TICKET_RESSOURCES (
     fk_ressource INT,
@@ -152,10 +168,10 @@ CREATE TABLE GLPI_TEST.TICKET_RESSOURCES (
     FOREIGN KEY (fk_ressource) REFERENCES GLPI_TEST.RESSOURCES(ressource_id), -- id de la ressouces correspondante dans la table 
     FOREIGN KEY (fk_ticket) REFERENCES GLPI_TEST.TICKETS(ticket_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
 
 
--- Table de liaison entre les commentaireset les ressources. Elle permet de gï¿½rer la relation 1-n entre les commentaires et les ressources.
+-- Table de liaison entre les commentaireset les ressources. Elle permet de gérer la relation 1-n entre les commentaires et les ressources.
 
 CREATE TABLE GLPI_TEST.COMMENT_RESSOURCES (
     fk_ressource INT,
@@ -164,10 +180,10 @@ CREATE TABLE GLPI_TEST.COMMENT_RESSOURCES (
     FOREIGN KEY (fk_ressource) REFERENCES GLPI_TEST.RESSOURCES(ressource_id),
     FOREIGN KEY (fk_comment) REFERENCES GLPI_TEST.COMMENTS(comment_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
 
 
--- Table de liaison entre les tickets et les users. Elle permet de gï¿½rer la relation n-n des observateurs des tickets.
+-- Table de liaison entre les tickets et les users. Elle permet de gérer la relation n-n des observateurs des tickets.
 
 CREATE TABLE GLPI_TEST.OBSERVERS (
     fk_ticket INT,
@@ -176,10 +192,10 @@ CREATE TABLE GLPI_TEST.OBSERVERS (
     FOREIGN KEY (fk_ticket) REFERENCES GLPI_TEST.TICKETS(ticket_id),
     FOREIGN KEY (fk_user) REFERENCES GLPI_TEST.USERS(user_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
 
 
--- Table de liaison entre les tickets et les users. Elle permet de gï¿½rer la relation n-n des responsables des tickets.
+-- Table de liaison entre les tickets et les users. Elle permet de gérer la relation n-n des responsables des tickets.
 
 CREATE TABLE GLPI_TEST.ASSIGNED_TO (
     fk_ticket INT,
@@ -188,6 +204,7 @@ CREATE TABLE GLPI_TEST.ASSIGNED_TO (
     FOREIGN KEY (fk_ticket) REFERENCES GLPI_TEST.TICKETS(ticket_id),
     FOREIGN KEY (fk_user) REFERENCES GLPI_TEST.USERS(user_id)
 );
--- les <fk_value> sont des INT et correspondent ï¿½ la valeur de lï¿½id de la table correspondante. Ce sont des clï¿½s ï¿½trangï¿½res liï¿½es ï¿½ d'autres tables, assurant l'intï¿½gritï¿½ rï¿½fï¿½rentielle entre les tables.
+-- les <fk_value> sont des INT et correspondent à la valeur de l’id de la table correspondante. Ce sont des clés étrangères liées à d'autres tables, assurant l'intégrité référentielle entre les tables.
+
 COMMIT;
-exit;
+EXIT;
