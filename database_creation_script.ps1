e6bdcc3005ab8ec2fa88bd1b8c28e30b72923c2d
Clément <82080806+clementcst@users.noreply.github.com>
2024-03-19 14:32:03 +0100

    Add files via upload

--- database_creation_script.ps1
+++ database_creation_script.ps1
@@ -19,6 +19,22 @@ $SqlplusPath = (Get-Command sqlplus.exe).Source
 $ScriptDir  = $(if ($psISE) {Split-Path -Path $psISE.CurrentFile.FullPath} else {$(if ($global:PSScriptRoot.Length -gt 0) {$global:PSScriptRoot} else {$global:pwd.Path})})
 $LogFileDirectory = "$($ScriptDir)\log\creation\"
 
+$TAD_PATH = "C:\TAD"
+$GLPI_DB_PROD_PATH = "C:\TAD\GLPI_DB_PROD"
+$GLPI_DB_TEST_PATH = "C:\TAD\GLPI_DB_TEST"
+$GLPI_ROLES_PATH = "C:\TAD\GLPI_ROLES"
+
+# Vérifie et crée les dossiers s'ils n'existent pas déjà
+$folders = @($TAD_PATH, $GLPI_DB_PROD_PATH, $GLPI_DB_TEST_PATH, $GLPI_ROLES_PATH)
+
+foreach ($folder in $folders) {
+    if (-not (Test-Path $folder)) {
+        New-Item -Path $folder -ItemType Directory -Force
+    }
+}
+
+
+
 if(-not(Test-Path -Path $LogFileDirectory)) { New-Item -ItemType Directory -Path $LogFileDirectory }
 
 # Définir les informations de connexion pour chaque préfixe de fichier
