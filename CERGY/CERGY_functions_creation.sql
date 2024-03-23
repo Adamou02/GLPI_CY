a11c8a3e78d7b8adc6545360f15caef962c23d48
Adam Bouhrara <42783707+Adamou02@users.noreply.github.com>
2024-03-23 16:01:05 +0100

    refac: Deplacement des appels de procedures dans un nouveau fichier

--- CERGY/CERGY_functions_creation.sql
+++ CERGY/CERGY_functions_creation.sql
@@ -287,28 +287,5 @@ BEGIN
 END REFRESH_VIEW;
 /
 
-
-
-
--- Appels des fonctions
-
-DECLARE
-    v_ticket GLPI_CERGY.GLOBAL_Ticket%ROWTYPE;
-BEGIN
-    v_ticket := GLPI_CERGY.GET_TICKET(1);
-    -- Afficher les champs du ticket rÃ©cupÃ©rÃ©
-    DBMS_OUTPUT.PUT_LINE('Ticket ID: ' || v_ticket.ticket_id);
-    DBMS_OUTPUT.PUT_LINE('Created By: ' || v_ticket.created_by);
-    -- Afficher d'autres champs si nÃ©cessaire
-END;
-/
-
-EXECUTE GLPI_CERGY.REFRESH_MATERIALIZED_VIEW('GLPI_CERGY.GLOBAL_TICKET');
-EXECUTE GLPI_CERGY.SET_TICKET_STATUS(1, 'To do');
-EXECUTE GLPI_CERGY.SOLVE_TICKET(1, 'Resolu avec la procedure');
-EXECUTE GLPI_CERGY.CLOSE_TICKET(1);
-EXECUTE GLPI_CERGY.ASSIGN_TICKET_TO_USER(1, 3);
-
-
 COMMIT;
 exit;
---
+++ PAU/PAU_functions_usage.sql
@@ -0,0 +1,18 @@
+-- Appels des fonctions
+
+DECLARE
+    v_ticket GLPI_PAU.GLOBAL_Ticket%ROWTYPE;
+BEGIN
+    v_ticket := GLPI_PAU.GET_TICKET(1);
+    -- Afficher les champs du ticket récupéré
+    DBMS_OUTPUT.PUT_LINE('Ticket ID: ' || v_ticket.ticket_id);
+    DBMS_OUTPUT.PUT_LINE('Created By: ' || v_ticket.created_by);
+    -- Afficher d'autres champs si nécessaire
+END;
+/
+
+EXECUTE GLPI_PAU.REFRESH_MATERIALIZED_VIEW('GLPI_PAU.GLOBAL_TICKET');
+EXECUTE GLPI_PAU.SET_TICKET_STATUS(1, 'To do');
+EXECUTE GLPI_PAU.SOLVE_TICKET(1, 'Resolu avec la procedure');
+EXECUTE GLPI_PAU.CLOSE_TICKET(1);
+EXECUTE GLPI_PAU.ASSIGN_TICKET_TO_USER(1, 3);
