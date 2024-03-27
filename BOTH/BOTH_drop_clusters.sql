88ab5a82acc2d0455e620a041ce523a3a3c11f73
LegJo <106883191+LegJo@users.noreply.github.com>
2024-03-27 10:47:24 +0100

    add lil comment BOTH_drop_clusters.sql

--- BOTH/BOTH_drop_clusters.sql
+++ BOTH/BOTH_drop_clusters.sql
@@ -1,12 +1,12 @@
 ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
 
--- Suppression des clusters de GLPI_CERGY
+-- Suppression des clusters de GLPI_CERGY et de leur index
 DROP INDEX GLPI_CERGY.idx_user_cluster;
 DROP INDEX GLPI_CERGY.idx_ticket_cluster;
 DROP CLUSTER  GLPI_CERGY.user_cluster;
 DROP CLUSTER GLPI_CERGY.ticket_cluster;
 
--- Suppression des clusters de GLPI_PAU
+-- Suppression des clusters de GLPI_PAU et de leur index
 DROP INDEX GLPI_PAU.idx_user_cluster;
 DROP INDEX GLPI_PAU.idx_ticket_cluster;
 DROP CLUSTER  GLPI_PAU.user_cluster;
